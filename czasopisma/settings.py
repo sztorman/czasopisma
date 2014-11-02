49de00dd3768abf438fd2a8fbf05fc7ec911c933
sztorman <michal.uziak@gmail.com>
2014-11-02 21:32:41 +0100

    Drugi commit

--- czasopisma/settings.py
+++ czasopisma/settings.py
@@ -14,8 +14,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'czasopisma_7',                      # Or path to database file if using sqlite3.
         # The folyelowing settings are not used with sqlite3:
-        'USER': 'uziak',
-        'PASSWORD': 'bye2x4fr',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
